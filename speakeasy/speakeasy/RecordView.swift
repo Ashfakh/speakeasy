f2014c70737472bcbaa4a46fdffbecab21aac3be
Ashfakh <ashfakhrithu@gmail.com>
2023-10-13 18:20:00 +0530

    aummarize function added

--- speakeasy/speakeasy/RecordView.swift
+++ speakeasy/speakeasy/RecordView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 import AVFoundation
+import Speech
 
 struct RecordAudioView: View {
     @State private var audioRecorder: AVAudioRecorder?
     @State private var audioPlayer: AVAudioPlayer?
     @State private var isRecording = false
     @State private var isPlaying = false
+    @State private var isPaused = false
     @State private var elapsedTime: TimeInterval = 0
     @State private var timer: Timer?
     @State private var showMessage = false
@@ -25,36 +27,37 @@ struct RecordAudioView: View {
         return documentPath.appendingPathComponent(uniqueName)
     }
     
+
     var body: some View {
         VStack(spacing: 20) {
             Button(action: {
                 if isRecording {
-                    stopRecording()
+                    if isPaused {
+                        continueRecording()
+                    } else {
+                        pauseRecording()
+                    }
                 } else {
                     startRecording()
                 }
             }) {
-                Text(isRecording ? "Stop Recording" : "Start Recording")
+                Text(isRecording ? (isPaused ? "Continue Recording" : "Pause Recording") : "Start Recording")
                     .foregroundColor(.white)
                     .padding()
                     .background(Color.red)
                     .cornerRadius(8)
             }
-
+            
             Button(action: {
-                if isPlaying {
-                    stopPlayback()
-                } else {
-                    startPlayback()
-                }
+                stopRecording()
             }) {
-                Text(isPlaying ? "Stop Playback" : "Play Recording")
+                Text("Stop Recording")
                     .foregroundColor(.white)
                     .padding()
-                    .background(Color.blue)
+                    .background(Color.gray)
                     .cornerRadius(8)
             }
-            .disabled(!FileManager.default.fileExists(atPath: audioURL.path))
+            .disabled(!isRecording)
 
             Text("Elapsed Time: \(Int(elapsedTime)) seconds")
 
@@ -72,69 +75,165 @@ struct RecordAudioView: View {
     }
 
     func startRecording() {
-            let audioSession = AVAudioSession.sharedInstance()
-
-            do {
-                try audioSession.setCategory(.record, mode: .default)
-                try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
-
-                let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-                let audioURL = documentPath.appendingPathComponent("audioRecording.m4a")
-
-                let settings = [
-                    AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
-                    AVSampleRateKey: 12000,
-                    AVNumberOfChannelsKey: 1,
-                    AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
-                ]
-
-                audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
-                audioRecorder?.record()
-
-                isRecording = true
-
-                // Start timer
-                timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                    elapsedTime += 1
+        let microphoneStatus = AVCaptureDevice.authorizationStatus(for: .audio)
+        if microphoneStatus != .authorized {
+            AVCaptureDevice.requestAccess(for: .audio) { granted in
+                if granted {
+                    self.checkAndRequestSpeechRecognitionPermission()
+                } else {
+                    // Handle microphone permission denial here, e.g., show an alert to the user
                 }
-
-            } catch {
-                print("Failed to start recording: \(error.localizedDescription)")
             }
-        }
-
-        func stopRecording() {
-            audioRecorder?.stop()
-            isRecording = false
-
-            // Stop and reset timer
-            timer?.invalidate()
-            timer = nil
-            
-            elapsedTime = 0
-            
-            let relativePath = audioURL.lastPathComponent
-            recordingDB.addRecording(name: "audioRecording-\(Date().timeIntervalSince1970)", filePath: relativePath)
-
-            // Show the saved message
-            showMessage = true
-            
-        }
-    
-    func startPlayback() {
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
-            audioPlayer?.play()
-            isPlaying = true
-        } catch {
-            print("Failed to play recording: \(error.localizedDescription)")
+        } else {
+            self.checkAndRequestSpeechRecognitionPermission()
         }
     }
 
-    func stopPlayback() {
-        audioPlayer?.stop()
-        isPlaying = false
-    }}
+    func checkAndRequestSpeechRecognitionPermission() {
+        let authStatus = SFSpeechRecognizer.authorizationStatus()
+        switch authStatus {
+        case .notDetermined:
+            SFSpeechRecognizer.requestAuthorization { status in
+                DispatchQueue.main.async {
+                    if status == .authorized {
+                        self.actuallyStartRecording()
+                    } else {
+                        // Handle speech recognition permission denial here
+                    }
+                }
+            }
+        case .authorized:
+            self.actuallyStartRecording()
+        default:
+            // Handle other cases here
+            break
+        }
+    }
+
+    func actuallyStartRecording() {
+        let audioSession = AVAudioSession.sharedInstance()
+
+        do {
+            try audioSession.setCategory(.record, mode: .default)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 12000,
+                AVNumberOfChannelsKey: 1,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+
+            audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            audioRecorder?.record()
+
+            isRecording = true
+
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                elapsedTime += 1
+            }
+
+        } catch {
+            print("Failed to start recording: \(error.localizedDescription)")
+        }
+    }
+    func stopRecording() {
+        audioRecorder?.stop()
+        isRecording = false
+        isPaused = false
+
+        // Stop and reset timer
+        timer?.invalidate()
+        timer = nil
+        elapsedTime = 0
+
+        transcribeAudioToText()
+    }
+
+    func transcribeAudioToText() {
+        guard let audioURL = audioRecorder?.url else {
+            print("Audio URL not found!")
+            return
+        }
+
+        let recognizer = SFSpeechRecognizer()
+        let request = SFSpeechURLRecognitionRequest(url: audioURL)
+
+        //TODO: Fix this
+//        recognizer?.recognitionTask(with: request) { (result, error) in
+//            guard let result = result else {
+//                print("There was an error: \(error?.localizedDescription ?? "Unknown error")")
+//                return
+//            }
+//
+//            // Get the transcribed text
+//            let transcribedText = result.bestTranscription.formattedString
+//
+//
+//            // Save the transcribed text to a file
+//            if let transcriptionURL = saveTranscriptionToFile(text: transcribedText) {
+//                      let filePathString = transcriptionURL.path
+//                      recordingDB.addRecording(name: "Recording on \(Date())", filePath: filePathString)
+//                  }
+//            removeAudioFile(at: audioURL)
+//
+//        }
+        
+        let transcribedText = "In a room filled with echoes, I stand alone, seeking solace in the silent corners of my mind. Every face I've passed, every voice I've heard, they meld into one distant murmur. The weight of existence bears down, reminding me that time is both an ally and enemy. Yet, in this quiet moment, I find clarity. Life is not about the vastness of years but the depth of experiences. Each fleeting second, every fleeting smile, is a testament to our resilience. I am not lost; I am on a journey. A journey that's uniquely mine. And I will find my way."
+
+
+            // Save the transcribed text to a file
+            if let transcriptionURL = saveTranscriptionToFile(text: transcribedText) {
+                      let filePathString = transcriptionURL
+                      recordingDB.addRecording(name: "Transcription on \(Date())", filePath: filePathString)
+                  }
+            removeAudioFile(at: audioURL)
+    }
+    
+    func saveTranscriptionToFile(text: String) -> String? {
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let uniqueName = "transcription-\(Date().timeIntervalSince1970).txt"
+        let fileURL = documentPath.appendingPathComponent(uniqueName)
+
+        do {
+            try text.write(to: fileURL, atomically: true, encoding: .utf8)
+            print("Transcription saved at \(uniqueName)")
+            return uniqueName
+        } catch {
+            print("Failed to save transcription: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
+
+    
+    func removeAudioFile(at url: URL) {
+        do {
+            try FileManager.default.removeItem(at: url)
+            print("Audio file removed successfully!")
+        } catch {
+            print("Failed to remove audio file: \(error.localizedDescription)")
+        }
+    }
+    
+    func pauseRecording() {
+        audioRecorder?.pause()
+        isPaused = true
+        
+        // Pause the timer
+        timer?.invalidate()
+    }
+
+    func continueRecording() {
+        audioRecorder?.record()
+        isPaused = false
+        
+        // Restart the timer
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            elapsedTime += 1
+        }
+    }
+}
 
 
 struct RecordAudioView_Previews: PreviewProvider {
--- speakeasy/speakeasy/RecordingListView.swift
+++ speakeasy/speakeasy/RecordingListView.swift
@@ -7,21 +7,14 @@
 
 import Foundation
 import SwiftUI
-import AVFoundation
 
 struct RecordingListView: View {
     @EnvironmentObject var recordingDB: RecordingDatabase
-    @State private var audioPlayer: AVAudioPlayer?
     
     var body: some View {
         List(recordingDB.recordings, id: \.id) { record in
-            HStack {
+            NavigationLink(destination: TranscriptionDetailView(transcription: getTranscription(for: record))) {
                 Text(record.name ?? "")
-                Spacer()
-                Button("Play") {
-                    playRecording(for:  record)
-                    // Play the recording using audioURL
-                }
             }
         }
         
@@ -34,16 +27,113 @@ struct RecordingListView: View {
         .cornerRadius(8)
     }
     
+    func getTranscription(for record: Recording) -> String {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let transcriptionURL = documentDirectory.appendingPathComponent(record.filePath ?? "")
+        
+        do {
+            let transcription = try String(contentsOf: transcriptionURL, encoding: .utf8)
+            return transcription
+        } catch {
+            print("Failed to read transcription: \(error.localizedDescription)")
+            return "Error reading transcription"
+        }
+    }
+}
+
+class OpenAIApiClient {
+    let baseURL = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+    let apiKey = "API_KEY" // Make sure to keep this secret and secure.
     
-    func playRecording(for record: Recording) {
-            let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            let audioURL = documentDirectory.appendingPathComponent(record.filePath ?? "")
+    func summarize(text: String, completion: @escaping (String?) -> Void) {
+        guard let url = URL(string: baseURL) else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
+        let body = [
+            "model": "gpt-3.5-turbo",
+            "messages": [
+                ["role": "system", "content": "Summarize the following text"],
+                ["role": "user", "content": text]
+            ]
+        ] as [String: Any]
+
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data, error == nil,
+                  let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+                  let dictionary = jsonObject as? [String: Any],
+                  let choices = dictionary["choices"] as? [[String: Any]],
+                  let firstChoice = choices.first,
+                  let text = firstChoice["text"] as? String
+            else {
+                completion(nil)
+                return
+            }
+
+            completion(text.trimmingCharacters(in: .whitespacesAndNewlines))
+        }.resume()
+    }
+}
+
+
+struct TranscriptionDetailView: View {
+    var transcription: String
+    @State private var summary: String?
+    @State private var showError: Bool = false
+
+    let openAIClient = OpenAIApiClient()
+
+    var body: some View {
+        VStack(spacing: 20) {
+            ScrollView {
+                Text(summary ?? transcription)
+                    .padding()
+            }
             
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: audioURL)
-                audioPlayer?.play()
-            } catch {
-                print("Failed to play recording: \(error.localizedDescription)")
+            if let summaryText = summary {
+                Text("Summary:")
+                    .font(.headline)
+                    .padding(.top, 10)
+                
+                Text(summaryText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+            }
+
+            if summary == nil { // Only display the "Summarize" button if the summary isn't generated yet.
+                Button("Summarize") {
+                    fetchSummary(for: transcription)
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(8)
             }
         }
+        .navigationBarTitle("Transcription", displayMode: .inline)
+        .alert(isPresented: $showError, content: {
+            Alert(title: Text("Error"), message: Text("Failed to fetch the summary. Please try again later."), dismissButton: .default(Text("OK")))
+        })
+    }
+    
+    func fetchSummary(for text: String) {
+        openAIClient.summarize(text: text) { receivedSummary in
+            DispatchQueue.main.async {
+                if let summaryText = receivedSummary {
+                    self.summary = summaryText
+                } else {
+                    self.showError = true
+                }
+            }
+        }
+    }
 }
+
+
+
